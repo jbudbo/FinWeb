36a9b569acb221aba94391085b97c666e268913d
Justin Morris <Justin.Morris@bd.com>
2025-01-22 15:13:57 -0500

    indexeddb started

---
+++ src/FinWeb/IndexedDbAccessor.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace FinWeb;
+
+public class IndexedDbAccessor(IJSRuntime jsRuntime)
+    : IAsyncDisposable
+{
+    private Lazy<IJSObjectReference> _accessorJsRef = new();
+
+    public async Task InitializeAsync()
+    {
+        await WaitForReference();
+        await _accessorJsRef.Value.InvokeVoidAsync("initialize");
+    }
+
+    public async Task<T> GetValueAsync<T>(string store, int id)
+    {
+        await WaitForReference();
+        return await _accessorJsRef.Value.InvokeAsync<T>("get", store, id);
+    }
+
+    public async Task SetValueAsync<T>(string store, T val)
+    {
+        await WaitForReference();
+
+        await _accessorJsRef.Value.InvokeVoidAsync("set", store, val);
+    }
+
+    private async Task WaitForReference()
+    {
+        if (_accessorJsRef.IsValueCreated is false)
+        {
+            _accessorJsRef = new(await jsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/IndexedDbAccessor.js"));
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        if (_accessorJsRef.IsValueCreated)
+        {
+            await _accessorJsRef.Value.DisposeAsync();
+        }
+    }
+}
---
+++ src/FinWeb/wwwroot/js/IndexedDbAccessor.js
@@ -0,0 +1,38 @@
+﻿export function initialize() {
+    const finDb = indexedDB.open(DB_NAME, DB_VERSION);
+    finDb.onupgradeneeded = function () {
+        const db = finDb.result;
+        db.createObjectStore("accounts", { keyPath: "name" });
+    }
+}
+
+export function set(store, value) {
+    const finDb = indexedDB.open(DB_NAME, DB_VERSION);
+    finDb.onsuccess = function () {
+        const transaction = finDb.result.transaction(store, "readwrite");
+        const collection = transaction.objectStore(store);
+        collection.put(value);
+    }
+}
+
+export async function get(store, id) {
+    const request = new Promise((resolve) => {
+        const finDb = indexedDB.open(DB_NAME, DB_VERSION);
+        finDb.onsuccess = function () {
+            const transaction = finDb.result.transaction(store, "readonly");
+            const collection = transaction.objectStore(store);
+            const result = collection.get(id);
+
+            result.onsuccess = function (e) {
+                resolve(result.result);
+            }
+        }
+    });
+
+    const result = await request;
+
+    return result;
+}
+
+const DB_NAME = "FinWeb";
+const DB_VERSION = 1;
